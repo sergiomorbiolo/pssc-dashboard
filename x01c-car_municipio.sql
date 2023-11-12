c984b9bc856a6c4390779c8ecb9d46dba49c87fe
buscaterra <contato@buscaterra.com.br>
2023-11-12 18:38:13 -0300

    Coddige script x01c

--- x01c-car_municipio.sql
+++ x01c-car_municipio.sql
@@ -9,39 +9,57 @@ DO $$
 	DECLARE
 		x record;
 		y record;
+		z record;
 		munx varchar[];
 		numero int;
     BEGIN
-		FOR x IN
+		FOR z IN
 			SELECT
-					id, 
-					cod_municipio, 
-					municipio, 
-					uf, 
-					ST_Buffer(geom,-0.00001) as geom
-				FROM 
+					count(*) as maximo 
+				from 
 					psscx.car_mun
 		LOOP
-			munx:=null;
-			numero:=0;
-			FOR y IN
-				SELECT 
-						cd_mun as cod_munx
-					FROM
-						psscx.municipios m
-					WHERE
-						ST_Intersects(m.geom,x.geom)
+	
+	
+	
+			FOR x IN
+				SELECT
+						id, 
+						cod_municipio, 
+						municipio, 
+						uf, 
+						ST_Buffer(geom,-0.00001) as geom
+					FROM 
+						psscx.car_mun
+					ORDER BY 
+						id
 			LOOP
-				munx[numero]:=y.cod_munx;
-				numero:=numero+1;
+				munx:=null;
+				numero:=0;
+				FOR y IN
+					SELECT 
+							id as id,
+							cd_mun as cod_munx
+						FROM
+							psscx.municipios m
+						WHERE
+							ST_Intersects(m.geom,x.geom)
+				LOOP
+					munx[numero]:=y.cod_munx;
+					numero:=numero+1;
+				END LOOP;
+				UPDATE
+						psscx.car_mun
+					SET
+						municipios=munx
+					WHERE
+						cod_municipio=x.cod_municipio;
+				raise info '%/% - %', x.id, z.maximo, x.cod_municipio;
 			END LOOP;
-			UPDATE
-					psscx.car_mun
-				SET
-					municipios=munx
-				WHERE
-					cod_municipio=x.cod_municipio;
-			raise info '% %', x.cod_municipio, munx;
+		
+		
+		
+		
 		END LOOP;
 	END;
 $$;
