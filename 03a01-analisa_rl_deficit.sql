445e86883f75a20f55e2a6e1dfce4f3d60484efb
buscaterra <contato@buscaterra.com.br>
2023-11-17 00:24:40 -0300

    Versão alternativa do script 03a01

--- 03a01-analisa_rl_deficit.sql
+++ 03a01-analisa_rl_deficit.sql
@@ -7,66 +7,65 @@ do $$ begin	raise info '03a - Analisa Reserva legal';
 -- Recorta o polígono de Reserva Legal por propriedade rural
 -- ================================================================================
 
-DO $$
-	DECLARE
-		x record;
-		y record;
-		barra integer;
-		porcentagem text;
-		erro text;
+-- DO $$
+-- 	DECLARE
+-- 		x record;
+-- 		y record;
+-- 		barra integer;
+-- 		porcentagem text;
+-- 		erro text;
 
-    BEGIN
-		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_area;
-		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_deficit_area;
-		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_deficit_geom;
+--     BEGIN
+-- 		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_area;
+-- 		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_deficit_area;
+-- 		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_deficit_geom;
 		
-		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_area double precision;
-		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_deficit_area double precision;
-		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_deficit_geom geometry(Geometry,4674);
+-- 		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_area double precision;
+-- 		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_deficit_area double precision;
+-- 		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_deficit_geom geometry(Geometry,4674);
 
-		FOR y IN
-			SELECT
-					count(*) 	AS maximo
-				from
-					pssc.rl
-		LOOP
-			FOR x IN
-				SELECT 
-						rl.id		as id, 
-						rl.car		as rl_car, 
-						vn.car		as vn_car,
-						rl.geom		as rl_geom,
-						vn.geom		as vn_geom
-					FROM 
-						pssc.rl rl INNER JOIN
-						pssc.vn vn ON
-						rl.car=vn.car
-					ORDER BY
-						rl.id
+-- 		FOR y IN
+-- 			SELECT
+-- 					count(*) 	AS maximo
+-- 				from
+-- 					pssc.rl
+-- 		LOOP
+-- 			FOR x IN
+-- 				SELECT 
+-- 						rl.id		as id, 
+-- 						rl.car		as rl_car, 
+-- 						vn.car		as vn_car,
+-- 						rl.geom		as rl_geom,
+-- 						vn.geom		as vn_geom
+-- 					FROM 
+-- 						pssc.rl rl INNER JOIN
+-- 						pssc.vn vn ON
+-- 						rl.car=vn.car
+-- 					ORDER BY
+-- 						rl.id
 -- 					limit 100
-			LOOP
-				barra=((x.id*50)/y.maximo);
-				porcentagem=to_char(((x.id*100.00)/y.maximo)::numeric, '990D99');
-				RAISE INFO '%/% % %░%     %', x.id, y.maximo, porcentagem || '%', repeat('░',barra), repeat('▬',50-barra), x.rl_car;
+-- 			LOOP
+-- 				barra=((x.id*50)/y.maximo);
+-- 				porcentagem=to_char(((x.id*100.00)/y.maximo)::numeric, '990D99');
+-- 				RAISE INFO '%/% % %░%     %', x.id, y.maximo, porcentagem || '%', repeat('░',barra), repeat('▬',50-barra), x.rl_car;
 -- 				WITH temp AS (
 -- 					SELECT
 -- 						ST_Multi(ST_CollectionExtract(ST_Union(ST_Difference(x.rl_geom,x.vn_geom,0.000000001)), 3))		AS deficit
 -- 				)
-				UPDATE
-						pssc.rl
-					SET
+-- 				UPDATE
+-- 						pssc.rl
+-- 					SET
 -- 						rl_area=(ST_Area(x.rl_geom::geography)/10000),
 -- 						rl_deficit_area=(ST_Area(temp.deficit::geography)/10000),
 -- 						rl_deficit_geom=temp.deficit
-						rl_deficit_geom=ST_Difference(x.rl_geom,x.vn_geom)
 -- 					FROM
 -- 						temp
-					WHERE
-						car=x.rl_car;
-			END LOOP;
-		END LOOP;
-	END;
-$$;
+-- 					WHERE
+-- 						car=x.rl_car;
+-- 			END LOOP;
+-- 		END LOOP;
+-- 	END;
+-- $$;
 
 
 
@@ -75,4 +74,22 @@ $$;
 
 
 
+
+-- 		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_area;
+-- 		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_deficit_area;
+-- 		ALTER TABLE pssc.rl DROP COLUMN IF EXISTS rl_deficit_geom;
+		
+-- 		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_area double precision;
+-- 		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_deficit_area double precision;
+-- 		ALTER TABLE pssc.rl ADD COLUMN IF NOT EXISTS rl_deficit_geom geometry(Geometry,4674);
+
+
+				UPDATE
+						pssc.rl
+					SET
+						rl_deficit_geom=ST_Difference(rl.geom,vn.geom)
+					FROM
+						pssc.rl rl INNER JOIN
+						pssc.vn vn ON
+						rl.car=vn.car
 
