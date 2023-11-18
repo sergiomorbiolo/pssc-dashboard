b1f87b056e58d0d00866fab54b25701e3136cb12
buscaterra <contato@buscaterra.com.br>
2023-11-18 14:45:49 -0300

    Correção script 02a

--- 02a-recorta_rl_imoveis.sql
+++ 02a-recorta_rl_imoveis.sql
@@ -8,99 +8,83 @@ do $$ begin	raise info '02 - Recorta Reserva Legal
 DO $$
 	DECLARE
 		x record;
+		y record;
 		erro text;
-		tabela_fonte text;
-		tabela_alvo text;
+		tabela_fonte text:='rl';
+		tabela_alvo text:='reserva_legal';
 		municipiosx text;
     BEGIN
-		tabela_alvo='rl';
-		tabela_fonte='reserva_legal';
-		EXECUTE FORMAT ('
-			ALTER TABLE pssc.imoveis DROP COLUMN IF EXISTS %s_geom;
-			ALTER TABLE pssc.imoveis DROP COLUMN IF EXISTS %s_area;
-			ALTER TABLE pssc.imoveis DROP COLUMN IF EXISTS %s_erro;
-			ALTER TABLE pssc.imoveis DROP COLUMN IF EXISTS %s_deficit_geom;
-			ALTER TABLE pssc.imoveis DROP COLUMN IF EXISTS %s_deficit_area;
-		', tabela_alvo, tabela_alvo, tabela_alvo, tabela_alvo, tabela_alvo);
-		EXECUTE FORMAT ('
-			ALTER TABLE pssc.imoveis ADD COLUMN IF NOT EXISTS %s_geom geometry(Geometry,4674);
-			ALTER TABLE pssc.imoveis ADD COLUMN IF NOT EXISTS %s_area double precision;
-			ALTER TABLE pssc.imoveis ADD COLUMN IF NOT EXISTS %s_erro text;
-			ALTER TABLE pssc.imoveis ADD COLUMN IF NOT EXISTS %s_deficit_geom geometry(Geometry,4674);
-			ALTER TABLE pssc.imoveis ADD COLUMN IF NOT EXISTS %s_deficit_area double precision;
-		', tabela_alvo, tabela_alvo, tabela_alvo, tabela_alvo, tabela_alvo);
-		FOR x IN 
-			SELECT 
-					i.id			AS id,
-					i.car			AS car,
-					lower(i.uf)		AS uf, 
-					i.cod_municipio	AS cod_municipio,
-					i.municipios	AS municipios,
-					i.geom 			AS geom 
-				FROM 
-					pssc.imoveis i
-				ORDER BY
-					id
--- 				LIMIT 10
+		FOR y IN
+			SELECT uf FROM pssc.ufs ORDER BY uf
 		LOOP
-			BEGIN
-				municipiosx=array_to_string(x.municipios, ''',''');
-				EXECUTE format(
-					'UPDATE
-							pssc.imoveis
-						SET
-							%s_geom=temp.geom,
-							%s_erro=''ok''
-						FROM (
-							SELECT
-									ST_Union(ST_Intersection(a.geometry::geometry, %L))	as geom
-								FROM 
-									car_%s.%s_%s a
-								WHERE
-									ST_Intersects(a.geometry::geometry,%L)
-									AND a.cod_mun in (''%s'')
-						) as temp
-						WHERE
-							car=''%s''
-					',
-					tabela_alvo,
-					tabela_alvo,
-					x.geom,
-					x.uf,
-					x.uf,
-					tabela_fonte,
-					x.geom,
-					municipiosx,
-					x.car
-				);
-				RAISE INFO '% - %
-
-				', x.id, x.car;
-			EXCEPTION WHEN OTHERS THEN
+		
+		
+		
+			EXECUTE FORMAT ('
+				ALTER TABLE pssc.imoveis_%s DROP COLUMN IF EXISTS %s_geom;
+				ALTER TABLE pssc.imoveis_%s DROP COLUMN IF EXISTS %s_area;
+				ALTER TABLE pssc.imoveis_%s DROP COLUMN IF EXISTS %s_erro;
+				ALTER TABLE pssc.imoveis_%s DROP COLUMN IF EXISTS %s_deficit_geom;
+				ALTER TABLE pssc.imoveis_%s DROP COLUMN IF EXISTS %s_deficit_area;
+			', 
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo
+			);
+			EXECUTE FORMAT ('
+				ALTER TABLE pssc.imoveis_%s ADD COLUMN IF NOT EXISTS %s_geom geometry(Geometry,4674);
+				ALTER TABLE pssc.imoveis_%s ADD COLUMN IF NOT EXISTS %s_area double precision;
+				ALTER TABLE pssc.imoveis_%s ADD COLUMN IF NOT EXISTS %s_erro text;
+				ALTER TABLE pssc.imoveis_%s ADD COLUMN IF NOT EXISTS %s_deficit_geom geometry(Geometry,4674);
+				ALTER TABLE pssc.imoveis_%s ADD COLUMN IF NOT EXISTS %s_deficit_area double precision;
+			', 
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo,
+				y.uf, tabela_alvo
+			);
+			FOR x IN 
+				SELECT 
+						i.id			AS id,
+						i.car			AS car,
+						lower(i.uf)		AS uf, 
+						i.cod_municipio	AS cod_municipio,
+						i.municipios	AS municipios,
+						i.geom 			AS geom 
+					FROM 
+						pssc.imoveis i
+					WHERE
+						uf=upper(y.uf)
+					ORDER BY
+						id
+	-- 				LIMIT 10
+			LOOP
 				BEGIN
-					erro=SQLERRM;
 					municipiosx=array_to_string(x.municipios, ''',''');
 					EXECUTE format(
 						'UPDATE
-								pssc.imoveis
+								pssc.imoveis_%s
 							SET
 								%s_geom=temp.geom,
-								%s_erro=%L
+								%s_erro=''ok''
 							FROM (
 								SELECT
-										ST_Union(ST_Intersection(ST_Makevalid(a.geometry)::geometry, %L))	as geom
+										ST_Union(ST_Intersection(a.geometry::geometry, %L))	as geom
 									FROM 
 										car_%s.%s_%s a
 									WHERE
 										ST_Intersects(a.geometry::geometry,%L)
 										AND a.cod_mun in (''%s'')
-							)
+							) as temp
 							WHERE
 								car=''%s''
 						',
+						lower(y.uf),
 						tabela_alvo,
 						tabela_alvo,
-						erro,
 						x.geom,
 						x.uf,
 						x.uf,
@@ -110,34 +94,77 @@ DO $$
 						x.car
 					);
 					RAISE INFO '% - %
-					%
 
-					', x.id, x.car, erro;
-
+					', x.id, x.car;
 				EXCEPTION WHEN OTHERS THEN
 					BEGIN
 						erro=SQLERRM;
 						municipiosx=array_to_string(x.municipios, ''',''');
-						RAISE INFO '% - %
-
-						', x.id, x.car;
 						EXECUTE format(
 							'UPDATE
-									pssc.imoveis
+									pssc.imoveis_$s
 								SET
-									%s_geom=NULL,
+									%s_geom=temp.geom,
 									%s_erro=%L
+								FROM (
+									SELECT
+											ST_Union(ST_Intersection(ST_Makevalid(a.geometry)::geometry, %L))	as geom
+										FROM 
+											car_%s.%s_%s a
+										WHERE
+											ST_Intersects(a.geometry::geometry,%L)
+											AND a.cod_mun in (''%s'')
+								)
 								WHERE
 									car=''%s''
 							',
+							lower(y.uf),
 							tabela_alvo,
 							tabela_alvo,
 							erro,
+							x.geom,
+							x.uf,
+							x.uf,
+							tabela_fonte,
+							x.geom,
+							municipiosx,
 							x.car
 						);
-					END;					
+						RAISE INFO '% - %
+						%
+
+						', x.id, x.car, erro;
+
+					EXCEPTION WHEN OTHERS THEN
+						BEGIN
+							erro=SQLERRM;
+							municipiosx=array_to_string(x.municipios, ''',''');
+							RAISE INFO '% - %
+
+							', x.id, x.car;
+							EXECUTE format(
+								'UPDATE
+										pssc.imoveis_%s
+									SET
+										%s_geom=NULL,
+										%s_erro=%L
+									WHERE
+										car=''%s''
+								',
+								lower(y.uf),
+								tabela_alvo,
+								tabela_alvo,
+								erro,
+								x.car
+							);
+						END;					
+					END;
 				END;
-			END;
-		END LOOP;
+			END LOOP;
+			
+			
+			
+			
+		END LOOP;	
 	END;
 $$;
